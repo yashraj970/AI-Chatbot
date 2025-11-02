65616ddc53f7f36ec91f21adcdfed64f278e6fae
Yash Raj <112754137+yashraj970@users.noreply.github.com>
2025-11-02 15:37:45 +0530

    Initial commit ai chatbot app

---
+++ app/api/chat/route.ts
@@ -0,0 +1,30 @@
+import { streamText, UIMessage, convertToModelMessages } from "ai";
+import { google } from "@ai-sdk/google";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const {
+    messages,
+    model,
+    webSearch,
+  }: {
+    messages: UIMessage[];
+    model: string;
+    webSearch: boolean;
+  } = await req.json();
+
+  const result = streamText({
+    model: google("gemini-2.5-flash"),
+    messages: convertToModelMessages(messages),
+    system:
+      "You are a helpful assistant that can answer questions and help with tasks",
+  });
+
+  // send sources and reasoning back to the client
+  return result.toUIMessageStreamResponse({
+    sendSources: true,
+    sendReasoning: true,
+  });
+}
